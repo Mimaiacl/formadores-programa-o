b9c231e21d2ec8c76106caa3c027461fc6d91598
Mimaiacl <160976279+Mimaiacl@users.noreply.github.com>
2024-06-27 20:08:58 -0300

    .

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
-const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas =[
     {
  enunciado: "Quais são os sinais e sintomas mais comuns na depressão, e como podemos diferenciar a depressão de outras condições de saúde mental?",
 alternativas:[
  "Como podemos identificar sinais de depressão em comparação com outras condições de saúde mental, como ansiedade ou transtorno bipolar, em diferentes faixas etárias",
-}
 "Como a depressão afeta exclusivamente o sono e o apetite, sem influenciar outros aspectos da saúde mental",
-]
+],
     },
     {
  enunciado: "Você acha que o celular afasta as pessoas pessoalmente?",
